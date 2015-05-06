1a510a0caf0020a20f8990d07f21a96658b799f1
Marek Bielik <bielik@zulapost.net>
2015-05-06 21:29:19 +0200

    relative paths working

--- src/gui/MenuGui.java
+++ src/gui/MenuGui.java
@@ -47,7 +47,7 @@ public class MenuGui extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        LabyrinthL.setIcon(new javax.swing.ImageIcon("/home/marek/NetBeansProjects/Labyrinth/lib/media/Title.gif")); // NOI18N
+        LabyrinthL.setIcon(new javax.swing.ImageIcon(getClass().getResource("/media/Title.gif"))); // NOI18N
 
         numberOfPlayersBG.add(players2RB);
         players2RB.setText("2");
@@ -255,40 +255,7 @@ public class MenuGui extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_newGameRBActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MenuGui().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel LabyrinthL;
